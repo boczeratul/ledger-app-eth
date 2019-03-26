be61548a5f1d65b78e1a1133353a10e39276f7de
Hsuan Lee <boczeratul@gmail.com>
2019-03-26 06:54:41 +0000

    Clear

--- dep/tokens.d
+++
@@ -1,6 +0,0 @@
-obj/tokens.o: src/tokens.h \
-  /home/boczeratul/bolos-devenv/clang-arm-fropi/bin/../lib/clang/4.0.0/include/stdint.h \
-  /usr/include/stdint.h /usr/include/features.h \
-  /usr/include/stdc-predef.h /usr/include/sys/cdefs.h \
-  /usr/include/bits/wordsize.h /usr/include/gnu/stubs.h \
-  /usr/include/gnu/stubs-32.h /usr/include/bits/wchar.h
