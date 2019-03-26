78938b24192b09def50bf4e524c81fae4da116c1
Hsuan Lee <boczeratul@gmail.com>
2019-03-26 03:47:43 +0000

    Build new logo

---
+++ dep/os_printf.d
@@ -0,0 +1,14 @@
+obj/os_printf.o: \
+  /home/boczeratul/bolos-devenv/clang-arm-fropi/bin/../lib/clang/4.0.0/include/stdarg.h \
+  /usr/include/string.h /usr/include/features.h \
+  /usr/include/stdc-predef.h /usr/include/sys/cdefs.h \
+  /usr/include/bits/wordsize.h /usr/include/gnu/stubs.h \
+  /usr/include/gnu/stubs-32.h \
+  /home/boczeratul/bolos-devenv/clang-arm-fropi/bin/../lib/clang/4.0.0/include/stddef.h \
+  /usr/include/xlocale.h /home/boczeratul/nanos-secure-sdk/include/os.h \
+  /home/boczeratul/nanos-secure-sdk/include/bolos_target.h \
+  /home/boczeratul/bolos-devenv/clang-arm-fropi/bin/../lib/clang/4.0.0/include/stdint.h \
+  /usr/include/stdint.h /usr/include/bits/wchar.h \
+  /home/boczeratul/nanos-secure-sdk/include/os_apilevel.h \
+  /home/boczeratul/nanos-secure-sdk/include/cx.h \
+  /home/boczeratul/nanos-secure-sdk/include/cx_compliance_141.h
